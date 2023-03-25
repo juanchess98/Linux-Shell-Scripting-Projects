338fedcfa80bbd1c478c355d57880ddcff6f9d87
Juan <juanchess98@gmail.com>
2023-03-25 12:52:35 -0500

    While loop and  shift command

---
+++ localusers/luser-demo07.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Demostrate the use of shift and command loops.
+
+# Display the first three parameters.
+
+echo "Parameter 1: ${1}"
+echo "Parameter 2: ${2}"
+echo "Parameter 3: ${3}"
+echo
+
+# Loop through all the positional parameters
+
+while [[ "${#}" -gt 0 ]]
+do
+    echo "Number of parameters: ${#}"
+    echo "Parameter 1: ${1}"
+    echo "Parameter 2: ${2}"
+    echo "Parameter 3: ${3}"
+    echo
+    shift 
+done 
